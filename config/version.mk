e9bd4b7cdbf65f9d1047c5153204631f953f080e
Ido Ben-Hur <idoybh2@gmail.com>
2023-03-02 21:57:53 +0200

    config: Conditionally enable dev keys signing

--- config/version.mk
+++ config/version.mk
@@ -16,3 +16,17 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.build.version.device=$(TARGET_PRODUCT_SHORT) \
     ro.pb.build.version?=$(PIXELBUILDS_VERSION) \
     ro.pb.buildtype?=$(PB_BUILD_TYPE)
+
+# Signing
+ifeq (user,$(TARGET_BUILD_VARIANT))
+ifneq (,$(wildcard .android-certs/releasekey.pk8))
+PRODUCT_DEFAULT_DEV_CERTIFICATE := .android-certs/releasekey
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.oem_unlock_supported=1
+endif
+ifneq (,$(wildcard .android-certs/verity.pk8))
+PRODUCT_VERITY_SIGNING_KEY := .android-certs/verity
+endif
+ifneq (,$(wildcard .android-certs/otakey.x509.pem))
+PRODUCT_OTA_PUBLIC_KEYS := .android-certs/otakey.x509.pem
+endif
+endif
